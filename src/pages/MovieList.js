6be4cc96a58be2ad471dbb5530ebfe4a61523da6
Andre <andre.luiz.bianco@gmail.com>
2024-09-09 12:58:24 +0100

    initial

---
+++ src/pages/MovieList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const MovieList = () => {
+  return (
+    <main>
+      <div className="text-6xl text-red-100">
+        Movielist
+      </div>
+    </main>
+  )
+}
